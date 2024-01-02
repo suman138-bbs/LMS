bf1157837af0a8687d30ede6db3122b62c5c253d
suman138-138 <sumanbhandari9860@gmail.com>
2024-01-02 09:46:10 +0530

    Basic setup

---
+++ server/server.ts
@@ -0,0 +1,10 @@
+import { app } from './app';
+require("dotenv").config()
+
+
+
+//create our server
+
+app.listen(process.env.PORT, () => {
+    console.log(`server is connected with port ${process.env.PORT} `)
+})
